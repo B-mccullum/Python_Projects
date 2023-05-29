aad57d8821ffea8f09fb6a0153beaf3c6eecf593
B-mccullum <bmc2994@yahoo.com>
2023-05-29 19:13:43 +0000

    exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -4,7 +4,7 @@ purchase_days_ago = int(input('How many days ago have you purchased the item? ')
 is_used = input('Have you used the item at all [y/n]? ')
 is_broken = input('Has the item broken down on its own [y/n]? ')
  
- if(is_broken == 'y' or (purchase_days_ago <= 10 and is_used == 'n')):
-       print('You can get a refund.')
-   else:
-         print('You cannot get a refund.')
+if(is_broken == 'y' or (purchase_days_ago <= 10 and is_used == 'n')):
+    print('You can get a refund.')
+else:
+    print('You cannot get a refund.')
