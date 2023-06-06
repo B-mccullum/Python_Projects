58a12475460c1d9e63e529d99042c213c102be24
Brandon McCullum <129446092+B-mccullum@users.noreply.github.com>
2023-06-06 15:25:49 -0500

    Update Pytube_Video_Viewer_Auto.py

--- Pytube_Video_Viewer_Auto.py
+++ Pytube_Video_Viewer_Auto.py
@@ -1,20 +1,13 @@
-# for this code I used Pycharm in combination with Pytube to download the Youtube videos onto my local computer.
+from pytube import Youtube  # Import the Youtube class from the pytube library
+from sys import argv  # Import the argv module from the sys library
 
-from pytube import Youtube
-from sys import argv
+link = argv[1]  # Get the YouTube link from the command line arguments
+yt = Youtube(link)  # Create a new Youtube object with the provided link
 
-link = argv[1]
-yt = Youtube(link)
+print("Title: ", yt.title)  # Print the title of the YouTube video
 
-print("Title: ", yt.title)
+print("View: ", yt.views)  # Print the number of views of the YouTube video
 
-print("View: ", yt.views)
+yd = yt.streams.get_highest_resolution()  # Get the stream with the highest resolution
 
-yd = yt.streams.get_highest_resolution()
-
-yd.download()
-
-#insert the file path using forward slashes and single '' quotations in between the parentheses on line 15.
-
-# on the terminal command line enter the custom folder name (YT.py, for example) followed by the the link to the youtube video in '' single quotations that you want to save offline and press enter.
-# the youtube video will be downloaded into your local file and be available to watch.
+yd.download()  # Download the YouTube video using the highest resolution stream
